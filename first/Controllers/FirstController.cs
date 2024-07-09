7483a4b7813d819a63076f5517b511174986309d
prederyo <ifkaraca14@gmail.com>
2024-07-09 09:41:30 +0300

    first commit

---
+++ first/Controllers/FirstController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace first.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FirstController : ControllerBase
+    {
+        [HttpGet("hava")]
+        public IActionResult GetTemperature()
+        {
+            return Ok("Hava " + 25 + " Derece");
+        }
+
+        [HttpPost("topla")]
+        public IActionResult AddNumbers(int x, int y)
+        {
+            int topla = x + y;
+            return Ok(topla);
+        }   
+    }
+}
